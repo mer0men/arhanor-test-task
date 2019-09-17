3d121584e3d3cbf7fa248b20a76cc73ecab9080e
Meromen <meromen1375@gmail.com>
2019-09-17 19:05:20 +1000

    Fix build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/arhanor-test-task.git/'
+    ? '/arhanor-test-task/'
     : '/',
   productionSourceMap: false,
   filenameHashing: false,
